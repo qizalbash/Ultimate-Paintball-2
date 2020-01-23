b4812cbdef47059fa284da29d8a0ce02ed2e447e
L1ghtBeam <42229281+L1ghtBeam@users.noreply.github.com>
2020-01-22 23:47:53 -0800

    basic player movement and networking

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField] Behaviour[] componentsToDisable = null;
+    [SerializeField] GameObject[] gameObjectsToDisable = null;
+
+    GameObject sceneCamera;
+
+    private void Start()
+    {
+        if (isLocalPlayer)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            sceneCamera = GameObject.Find("SceneCamera");
+            sceneCamera.SetActive(false);
+        }
+        else
+        {
+            DisableComponents();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (isLocalPlayer)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            sceneCamera.SetActive(true);
+        }
+    }
+
+    void DisableComponents()
+    {
+        foreach (Behaviour _behaviour in componentsToDisable)
+        {
+            _behaviour.enabled = false;
+        }
+
+        foreach (GameObject _gameObject in gameObjectsToDisable)
+        {
+            _gameObject.SetActive(false);
+        }
+    }
+}
