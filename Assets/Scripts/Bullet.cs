06f5935cbc65b382d3c28e5d4d9593d438e08403
Nathan Woolsey <nwools@icloud.com>
2020-01-24 10:30:21 -0800

    Projectile behavior
    I made the projectile move in a strait line while being affected by gravity. The projectile destroys itself when a timer is reached or it collides with the ground.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed;
+    public float speed = 10.0f;
 
-    private void Update()
-    {
-        
+
+    void Start(){
+        speed = 20.0f;
+        StartCoroutine(DeathRoutine());
     }
-}
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "terrain"){
+            Destroy(gameObject);
+        }
+       
+    }
+
+    IEnumerator DeathRoutine(){
+        yield return new WaitForSeconds(10.0f);
+        Destroy(gameObject);
+    }
+    
+    }
